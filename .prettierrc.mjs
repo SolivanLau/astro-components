24197a1e286b3ddea372c917d0010c22cc600d87
solivan-lau <solivan_lau@outlook.com>
2024-12-20 10:48:40 -0500

    CONFIG: prettier installed and working for astro. content populated

---
+++ .prettierrc.mjs
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+
+export default {
+	semi: true,
+	singleQuote: false,
+	tabWidth: 4,
+	useTabs: true,
+	trailingComma: "es5",
+	plugins: ["prettier-plugin-astro"],
+	overrides: [
+		{
+			files: "*.astro",
+			options: {
+				parser: "astro",
+			},
+		},
+	],
+};
